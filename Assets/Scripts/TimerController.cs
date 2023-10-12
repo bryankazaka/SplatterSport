c5a0d63b83eca21ecf16382929bd035a63a0ef02
JustFran <43820050+JustFran27@users.noreply.github.com>
2023-10-12 19:13:30 +0200

    Fully Animated Brazier Timers
    Implemented the brazier animations for the timer made by Josh.

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -7,32 +8,34 @@ public class TimerController : MonoBehaviour
 {
     public int timerSeconds;
     public TileBase[] brazierStates;
-    private int state;
-    private (int,int) pos;
+    private Vector3Int pos;
     public Tilemap timerMap;
     // Start is called before the first frame update
     void Start()
     {
-        float secondsPerUpdate = ((float)timerSeconds)/(32.0f*3.0f); //calculate the amount of time between brazier tile updates
+        float secondsPerUpdate = ((float)timerSeconds)/(32.0f); //calculate the amount of time between brazier tile updates
 
-        state = 0;
-        pos = (-16,9); // the leftmost position of the braziers
+       
+        pos = new Vector3Int(15,8,0); // the leftmost position of the braziers
+        for (int i = 0; i < 32; i++)
+        {
+            pos.x -= 1;
+            timerMap.SetAnimationFrame(pos,Random.Range(0,9));
+            
+        }
+        pos = new Vector3Int(15,8,0);
         InvokeRepeating(nameof(Light), 0, secondsPerUpdate); //repeat the animation
     }
 
     void Light()
     {
-        state+=1; // set it to the next state, cycles between 1, 2, and 3 to go between the three brazier states
+       
         
-        timerMap.SetTile(new Vector3Int(pos.Item1,pos.Item2,0),brazierStates[state]); //set the tile for the brazier
+        timerMap.SetTile(pos,brazierStates[1]); //set the tile for the brazier
         
-        if (state == 3) //if its the final state move to the next tile and reset the states
-        {
-            pos.Item1 += 1;
-            state = 0;
-        }
+        pos.x -= 1;
         
-        if (pos.Item1 > 15) // when the timer runs out destroy the object {add end round function here}
+        if (pos.x < -16) // when the timer runs out destroy the object {add end round function here}
         {
             GameObject.Destroy(gameObject);
         }
