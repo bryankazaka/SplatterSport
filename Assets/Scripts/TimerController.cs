15a87fae1227b8d01ff88b6b63edec674f1e7476
JustFran <43820050+JustFran27@users.noreply.github.com>
2023-10-16 09:04:57 +0200

    Timer Fixed
    The timer for the background is fixed and shadows are added

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -13,17 +13,17 @@ public class TimerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float secondsPerUpdate = ((float)timerSeconds)/(32.0f); //calculate the amount of time between brazier tile updates
+        float secondsPerUpdate = ((float)timerSeconds)/(26.0f); //calculate the amount of time between brazier tile updates
 
        
-        pos = new Vector3Int(15,8,0); // the leftmost position of the braziers
-        for (int i = 0; i < 32; i++)
+        pos = new Vector3Int(12,8,0); // the leftmost position of the braziers
+        for (int i = 0; i < 26; i++)
         {
             pos.x -= 1;
             timerMap.SetAnimationFrame(pos,Random.Range(0,9));
             
         }
-        pos = new Vector3Int(15,8,0);
+        pos = new Vector3Int(12,8,0);
         InvokeRepeating(nameof(Light), 0, secondsPerUpdate); //repeat the animation
     }
 
@@ -35,7 +35,7 @@ public class TimerController : MonoBehaviour
         
         pos.x -= 1;
         
-        if (pos.x < -16) // when the timer runs out destroy the object {add end round function here}
+        if (pos.x < -13) // when the timer runs out destroy the object {add end round function here}
         {
             GameObject.Destroy(gameObject);
         }
