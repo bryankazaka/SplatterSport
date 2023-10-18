6b58c903af1e0f685d00d443b3b57a3f4ae6bfa2
JustFran <43820050+JustFran27@users.noreply.github.com>
2023-10-18 13:43:48 +0200

    Brazier Update
    The braziers have a startup animation

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -10,35 +10,64 @@ public class TimerController : MonoBehaviour
     public TileBase[] brazierStates;
     private Vector3Int pos;
     public Tilemap timerMap;
+    private float startUp;
+    private bool litUp;
     // Start is called before the first frame update
     void Start()
     {
         float secondsPerUpdate = ((float)timerSeconds)/(26.0f); //calculate the amount of time between brazier tile updates
-
-       
-        pos = new Vector3Int(12,8,0); // the leftmost position of the braziers
-        for (int i = 0; i < 26; i++)
-        {
-            pos.x -= 1;
-            timerMap.SetAnimationFrame(pos,Random.Range(0,9));
-            
-        }
+        litUp=false;
+        startUp = Time.time;
+        
         pos = new Vector3Int(12,8,0);
-        InvokeRepeating(nameof(Light), 0, secondsPerUpdate); //repeat the animation
+         //repeat the animation
     }
 
+    private void Update() 
+    {
+        if (Time.time - startUp > 0.7f)
+        {
+            if (!litUp)
+            {
+                Debug.Log("Round End");
+                litUp= true;
+                Vector3Int TempPos = new(12,8,0); // the leftmost position of the braziers
+                for (int i = 0; i < 26; i++)
+                {
+                    
+                    Debug.Log(brazierStates[0].name);
+                    
+                   
+                    timerMap.SetTile(TempPos,brazierStates[0]);
+
+                    timerMap.SetAnimationFrame(TempPos,Random.Range(0,9));
+                    TempPos.x -= 1;
+                    
+                }
+                float secondsPerUpdate = ((float)timerSeconds)/(26.0f);
+                InvokeRepeating(nameof(Light), secondsPerUpdate, secondsPerUpdate);
+            }
+                 
+        }
+       
+    }
     void Light()
     {
        
         
         timerMap.SetTile(pos,brazierStates[1]); //set the tile for the brazier
-        
+        timerMap.SetAnimationFrame(pos,0);
         pos.x -= 1;
         
         if (pos.x < -13) // when the timer runs out destroy the object {add end round function here}
         {
-            GameObject.Destroy(gameObject);
+            EndRound();
         }
+    }
+
+    void EndRound()
+    {
+        Debug.Log("Round End");
     }
 
 }
