78682787a49613b3d4ef1e1bb31efaeee1183168
JustFran <43820050+JustFran27@users.noreply.github.com>
2023-10-23 01:36:47 +0200

    Weapon Speed Fixes

--- Assets/Scripts/WeaponObjects/Roller.cs
+++ Assets/Scripts/WeaponObjects/Roller.cs
@@ -4,7 +4,7 @@ public class Roller : Weapon {
     public Roller(int colour) : base(colour) {
         this.damage = 15;
         this.range = 1.27f;
-        this.attackSpeed = 1f;
+        this.attackSpeed = 0.75f;
         
     }
 }
